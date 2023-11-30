f29894bfe2f0541aaefff32afddf54aede35a509
Pablo <pablomurbaez@yahoo.com>
2023-11-29 21:45:27 -0500

    completed lab

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+    // todo
+  }
+  function shout(string) {
+  return string;
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+  function shout(string) {
+  return string.toUpperCase();
+  }
+  function whisper(string) {
+  return string.toLowerCase();
+  }
+  function logShout (aWord) {
+    console.log (aWord.toUpperCase());
+    }
+    function logWhisper (aWord) {
+      console.log (aWord.toLowerCase());
+      }
+    function sayHiToHeadphonedRoommate(string) {
+      if (string.toLowerCase() === string) {
+        return "I can't hear you!";
+      } else if (string.toUpperCase() === string) {
+          return "YES INDEED!";
+      } else if (string === "Let's have dinner together!") {
+          return "I would love to!";
+    }
+  }
